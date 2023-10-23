664208b932347033ecf0cc76796324b6dabf1e0b
MInealex <alejandrosanchezpinto@gmail.com>
2023-10-23 18:54:37 +0200

    Ejercicio hecho

--- Tema 1/Ejercicio-1/app/src/main/java/com/example/di_dam2/MainActivity.java
+++ Tema 1/Ejercicio-1/app/src/main/java/com/example/di_dam2/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.di_dam2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,4 +21,22 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_new_player);
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_buscar) {
+            Toast.makeText(this, "Buscar", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
