8e32954b1fe4ef9d531477862c5d3c5615d7ce03
Minealex <alejandrosanchezpinto@gmail.com>
2023-11-10 19:13:07 +0100

    Solo falta que funcione

--- Tema 1/Ejercicio-1/app/src/main/java/com/example/di_dam2/About.java
+++ Tema 1/Ejercicio-1/app/src/main/java/com/example/di_dam2/About.java
@@ -1,59 +1,40 @@
 package com.example.di_dam2;
+import android.os.Bundle;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.di_dam2.about.AboutAdapter;
 
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import com.google.android.material.bottomappbar.BottomAppBar;
-
-
-import java.util.ArrayList;
 import java.util.List;
 
+public class About extends AppCompatActivity {
 
+    private final List<Integer> images = List.of(
+            R.mipmap.games_aircontrol_foreground,
+            R.mipmap.games_angrybirds_foreground,
+            R.mipmap.games_dragonfly_foreground,
+            R.mipmap.games_hillclimbingracing_foreground,
+            R.mipmap.games_ninijump_foreground,
+            R.mipmap.games_pocketsoccer_foreground,
+            R.mipmap.games_radiantdefense_foreground
+    );
 
-public class About extends AppCompatActivity{
-
-    private ListView listView;
-    private List<String> generos = new ArrayList<>() {{
-        add("Plataformas");
-        add("Aventura");
-        add("Estrategia");
-        add("Deportes");
-        add("Carreras");
-        add("Shooter");
-        add("Lucha");
-        add("Simulación");
-        add("Rol");
-        add("Puzzle");
-        add("Musical");
-        add("Educativo");
-        add("Otros");
-
-    }};
-    private ArrayAdapter<String> adaptador;
-
+    private final List<String> names = List.of("Javier", "Jorge", "Jordi", "Jose", "Juan", "Julia", "Julian");
+    private final List<String> years = List.of("1990", "1991", "1992", "1993", "1994", "1995", "1996");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.about);
 
-        listView = findViewById(R.id.generosListView);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
-        adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, generos);
-        listView.setAdapter(adaptador);
-
-        BottomAppBar bottomAppBar = findViewById(R.id.bottomAppBar);
-        var fab = findViewById(R.id.fab);
-
-        bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_CENTER);
+        AboutAdapter adapter = new AboutAdapter(images, names, years, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
 
     }
-
-
 }
---
+++ Tema 1/Ejercicio-1/app/src/main/java/com/example/di_dam2/about/AboutViewHolder.java
@@ -0,0 +1,28 @@
+package com.example.di_dam2.about;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.di_dam2.R;
+
+
+public class AboutViewHolder extends RecyclerView.ViewHolder {
+
+    protected ImageView image;
+    protected TextView title;
+    protected TextView subtitle;
+    protected LinearLayout layout;
+
+    public AboutViewHolder(@NonNull View itemView) {
+        super(itemView);
+        image = itemView.findViewById(R.id.aboutImageView);
+        title = itemView.findViewById(R.id.aboutTitle);
+        subtitle = itemView.findViewById(R.id.subtitle);
+        layout = itemView.findViewById(R.id.linear);
+    }
+}
