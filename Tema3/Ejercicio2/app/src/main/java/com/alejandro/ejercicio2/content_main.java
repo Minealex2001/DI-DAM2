23b524da2c983ad60d49f470ebc576ef151644a4
Minealex <alejandrosanchezpinto@gmail.com>
2024-01-15 19:18:44 +0100

    Ejercicio 2

---
+++ Tema3/Ejercicio2/app/src/main/java/com/alejandro/ejercicio2/content_main.java
@@ -0,0 +1,14 @@
+package com.alejandro.ejercicio2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class content_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+    }
+}
